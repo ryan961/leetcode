aea17d04a457063a743118647b7a71523a37cf2a
GGu <lei.gu@mybogames.com>
2023-08-30 17:25:47 +0800

    feat: add solution and problem statement for "Longest Substring Without Repeating Characters" problem
    - Add a new file `[3]Longest Substring Without Repeating Characters.go` with the implementation of the solution for the problem "Longest Substring Without Repeating Characters"
    - Add a new file `[3]Longest Substring Without Repeating Characters.md` with the problem statement and examples
    - Add a new file `wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-a3mr2.lcv` with the explanation of the solution for the problem
    - Add a new file `wu-zhong-fu-zi-fu-de-zui-chang-zi-chuan-by-leetc-2.lcv` with a video explanation and a text explanation of the solution for the problem
    
    Signed-off-by: GGu <lei.gu@mybogames.com>

---
+++ editor/cn/[3]Longest Substring Without Repeating Characters.go
@@ -0,0 +1,65 @@
+package main
+
+import "strings"
+
+// [3] Longest Substring Without Repeating Characters ( longest-substring-without-repeating-characters )
+//Given a string s, find the length of the longest substring without repeating
+//characters.
+//
+//
+// Example 1:
+//
+//
+//Input: s = "abcabcbb"
+//Output: 3
+//Explanation: The answer is "abc", with the length of 3.
+//
+//
+// Example 2:
+//
+//
+//Input: s = "bbbbb"
+//Output: 1
+//Explanation: The answer is "b", with the length of 1.
+//
+//
+// Example 3:
+//
+//
+//Input: s = "pwwkew"
+//Output: 3
+//Explanation: The answer is "wke", with the length of 3.
+//Notice that the answer must be a substring, "pwke" is a subsequence and not a
+//substring.
+//
+//
+//
+// Constraints:
+//
+//
+// 0 <= s.length <= 5 * 10⁴
+// s consists of English letters, digits, symbols and spaces.
+//
+//
+// Related Topics 哈希表 字符串 滑动窗口 👍 9552 👎 0
+
+// leetcode submit region begin(Prohibit modification and deletion)
+func lengthOfLongestSubstring(s string) int {
+	var longest, current string
+
+	for k, v := range s {
+		if strings.Contains(s[:k], string(v)) {
+			current = current[strings.Index(current, string(v))+1:]
+		}
+
+		current += string(v)
+
+		if len(current) >= len(longest) {
+			longest = current
+		}
+	}
+
+	return len(longest)
+}
+
+//leetcode submit region end(Prohibit modification and deletion)
